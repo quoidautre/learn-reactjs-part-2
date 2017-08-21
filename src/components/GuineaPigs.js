d1a5bcea9efa027d483f97de4147e25ce954976d
Fabrice Pestre <developpement.fabrice@riseup.net>
2017-08-21 22:48:11 +0200

    statless mode

--- src/components/GuineaPigs.js
+++ src/components/GuineaPigs.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-export class GuineaPigs extends React.Component {
-    render() {
-        const src = this.props.src;
-        return (
-            <div>
-                <h1>Cute Guinea Pigs</h1>
-                <img src={src} />
-            </div>
-        );
-    }
+export const GuineaPigs = (props) => {
+    let src = props.src;
+    return (
+        <div>
+            <h1>Cute Guinea Pigs</h1>
+            <img src={src} />
+        </div>
+    );
 }
